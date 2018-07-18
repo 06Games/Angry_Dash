54426cf7edfd4ccfc3f2d59cf60ff66a2f6d72d1
EvanG <evan_g@orange.fr>
2018-07-18 12:04:18 +0200

    Suppression de fichiers devenus inutiles

--- Assets/Scripts/Particules.cs
+++ Assets/Scripts/Particules.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Particules : MonoBehaviour {
+public class Particules : MonoBehaviour
+{
 
     public int RotateSpeed = 25;
     public int FallSpeed = 5;
     public Vector2 FallDirection = new Vector3(0, 1);
     public bool Static;
 
-    void Update () {
-        if(!Static)
+    void Update()
+    {
+        if (!Static)
             transform.Translate(new Vector2(FallDirection.x * FallSpeed, FallDirection.y * FallSpeed));
-        else transform.position = new Vector3(transform.position.x, transform.position.y-FallSpeed);
-        transform.Rotate(new Vector3(0, 0, RotateSpeed/100F));
+        else transform.position = new Vector3(transform.position.x, transform.position.y - FallSpeed);
+        transform.Rotate(new Vector3(0, 0, RotateSpeed / 100F));
         if (transform.position.y < -500)
             Destroy(gameObject);
     }
