04afceb39d95de824aa07e31076cf2f2f163b4e3
EvanG <evan_g@orange.fr>
2018-07-26 15:09:45 +0200

    Changement de la façon de changer la couleur d'un bloc, nouveau ColorPicker

--- Assets/Scripts/Editeur/BlocColor.cs
+++ Assets/Scripts/Editeur/BlocColor.cs
@@ -5,58 +5,23 @@ using UnityEngine.UI;
 
 public class BlocColor : MonoBehaviour {
 
-    public InputField[] RgbField;
-    public Scrollbar[] RgbBar;
-    byte[] RGB = new byte[4];
-
     public Editeur editeur;
     public int Bloc = -1;
+    ColorPicker CP;
+
     private void Start()
     {
-        RGB = new byte[] { 255, 255, 255, 255 };
-        RgbField[0].text = RGB[0].ToString();
-        RgbField[1].text = RGB[1].ToString();
-        RgbField[2].text = RGB[2].ToString();
-        RgbField[3].text = RGB[3].ToString();
+        CP = transform.GetChild(0).GetComponent<ColorPicker>();
+        if (string.IsNullOrEmpty(editeur.file)) gameObject.SetActive(false);
     }
 
-    void Update () {
+    private void Update()
+    {
         if (Bloc != editeur.SelectedBlock)
         {
             Bloc = editeur.SelectedBlock;
-
-            Color32 a = Editeur.HexToColor(editeur.GetBlocStatus(3, Bloc));
-            RGB = new byte[] { a.r, a.g, a.b, a.a };
-
-            for (int i = 0; i < RgbField.Length; i++)
-            {
-                RgbField[i].text = RGB[i].ToString();
-                RgbBar[i].value = float.Parse(RGB[i].ToString()) / 255;
-            }
+            CP.CurrentColor = Editeur.HexToColor(editeur.GetBlocStatus(3, Bloc));
         }
-        else
-        {
-            for (int i = 0; i < RgbField.Length; i++)
-            {
-                if (RgbField[i].text != RgbBar[i].value.ToString() & RgbField[i].text == RGB[i].ToString())
-                    RgbField[i].text = ((int)(RgbBar[i].value * 255)).ToString();
-
-                if (RgbField[i].text != ((int)(RgbBar[i].value * 255)).ToString() & RgbField[i].text != "")
-                    RgbBar[i].value = int.Parse(RgbField[i].text) / 255;
-            }
-
-            if (RgbField[0].text != "")
-                RGB[0] = byte.Parse(RgbField[0].text);
-            if (RgbField[1].text != "")
-                RGB[1] = byte.Parse(RgbField[1].text);
-            if (RgbField[2].text != "")
-                RGB[2] = byte.Parse(RgbField[2].text);
-            if (RgbField[3].text != "")
-                RGB[3] = byte.Parse(RgbField[3].text);
-
-            Color32 color = new Color32(RGB[0], RGB[1], RGB[2], RGB[3]);
-            GetComponent<Image>().color = color;
-            editeur.ChangBlocStatus(3, Editeur.ColorToHex(color), Bloc);
-        }
+        else editeur.ChangBlocStatus(3, Editeur.ColorToHex(CP.CurrentColor), Bloc);
     }
 }
