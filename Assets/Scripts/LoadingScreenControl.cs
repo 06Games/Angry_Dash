560938c1ea7d95e800bca741465569f03ce8b438
EvanG <evan_g@orange.fr>
2018-07-30 22:39:57 +0200

    Nouvelle Ecran de Chargement

--- Assets/Scripts/LoadingScreenControl.cs
+++ Assets/Scripts/LoadingScreenControl.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class LoadingScreenControl : MonoBehaviour {
     public GameObject loadingScreenObj;
-    public Slider slider; 
+    public Slider slider;
+    public Sprite[] Backgrounds;
     AsyncOperation async;
 
     public void LoadScreen(string Scene) {
         loadingScreenObj = transform.GetChild(0).gameObject;
         slider = loadingScreenObj.transform.GetChild(0).GetComponent<Slider>();
+
+        System.Random rnd = new System.Random();
+        int i = rnd.Next(0, Backgrounds.Length);
+        if (i >= Backgrounds.Length)
+            i = Backgrounds.Length - 1;
+        loadingScreenObj.transform.GetChild(2).GetChild(0).GetComponent<Image>().sprite = Backgrounds[i];
 
         StartCoroutine(LoadingScreen(Scene));
     }
