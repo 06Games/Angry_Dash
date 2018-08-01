6c3b45f112163b8b039cf841b958d30e9617d778
EvanG <evan_g@orange.fr>
2018-08-01 18:24:35 +0200

    Possibilité de changer de menu dans l'editeur avec les touches du clavier
    Build : 1
    Edit : 2
    Delete : 3 ou Suppr.
    Events : 4
    Option : 5

---
+++ Assets/MenuByKey.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CreatorManager))]
+public class MenuByKey : MonoBehaviour
+{
+
+    public int[] menu;
+    public string[] keys;
+
+    void Update()
+    {
+        for (int i = 0; i < menu.Length; i++)
+        {
+            string[] k = keys[i].Split(new string[] { " | " }, System.StringSplitOptions.None);
+            for (int v = 0; v < k.Length; v++)
+            {
+                KeyCode key = (KeyCode)System.Enum.Parse(typeof(KeyCode), k[v]);
+                if (Input.GetKeyDown(key))
+                    GetComponent<CreatorManager>().array = menu[i];
+            }
+        }
+    }
+}
