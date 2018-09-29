701f3b5c440907d746ce501956d2be1babe2d40b
EvanG <evan_g@orange.fr>
2018-09-29 18:40:13 +0200

    Trigger Move : Mouvement Fluide

--- Assets/Scripts/Components/Triggers/MoveTrigger.cs
+++ Assets/Scripts/Components/Triggers/MoveTrigger.cs
@@ -25,8 +25,19 @@ public class MoveTrigger : MonoBehaviour
             for (int b = 0; b < Blocks.Length; b++)
             {
                 GameObject go = GameObject.Find("Objet n° " + Blocks[b]);
-                if (go != null & Speed >= 1) go.transform.Translate((Range / Speed) * 50);
-                if (go.GetComponent<Mur>() != null) go.GetComponent<Mur>().Move = Range / Speed;
+
+                Vector2 moveVector = new Vector2();
+                if (Type == 0) moveVector = Range / Speed;
+                else if (Type == 1)
+                {
+                    float v = i + 1;
+                    if (i > Speed / 2)
+                        v = Speed - i;
+                    moveVector = (Range / Speed) * (v / ((Speed/2)+1)) * 2;
+                }
+
+                if (go != null & Speed >= 1) go.transform.Translate(moveVector * 50);
+                if (go.GetComponent<Mur>() != null) go.GetComponent<Mur>().Move = moveVector;
             }
 
             yield return new WaitForEndOfFrame();
