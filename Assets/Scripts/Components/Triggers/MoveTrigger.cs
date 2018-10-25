50e47f089f204cf4d6312aa15e6643f47e10f0dc
EvanG <evan_g@orange.fr>
2018-10-25 11:55:22 +0200

    Bug fix : Le calcule du reset du Move est plus fiable + Optimisation du Move

--- Assets/Scripts/Components/Triggers/MoveTrigger.cs
+++ Assets/Scripts/Components/Triggers/MoveTrigger.cs
@@ -17,7 +17,6 @@ public class MoveTrigger : MonoBehaviour
 
     bool Used = false;
 
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (!Used | MultiUsage)
@@ -26,34 +25,49 @@ public class MoveTrigger : MonoBehaviour
 
     IEnumerator Move()
     {
-        int FPS = int.Parse(GameObject.Find("Base").transform.GetChild(1).GetComponent<Text>().text.Replace(" FPS", ""));
+        GameObject[] go = null;
+        if (AffectationType == 0)
+        {
+            go = new GameObject[Blocks.Length];
+            for (int b = 0; b < Blocks.Length; b++)
+                go[b] = GameObject.Find("Objet n° " + Blocks[b]);
+        }
+        else if (AffectationType == 1)
+        {
+            go = new GameObject[] { GameObject.Find("Main Camera") };
+            go[0].GetComponent<MainCam>().OnPlayer = false;
+        }
+
+        Vector3 affectedPos = new Vector3();
+        Vector3 affectedRot = new Vector3();
+        if (go.Length > 0 & Blocks.Length > 0)
+        {
+            if (AffectationType == 0)
+            {
+                try { affectedPos = GameObject.Find("Main Camera").GetComponent<LevelPlayer>().GetObjectPos(int.Parse(Blocks[0])); } catch { }
+                try { affectedRot = Editor_MoveTrigger.getVector3(GameObject.Find("Main Camera").GetComponent<LevelPlayer>().GetBlocStatus("Rotation", int.Parse(Blocks[0]))); } catch { }
+            }
+            else if (AffectationType == 1)
+            {
+                affectedPos = GameObject.Find("Main Camera").GetComponent<MainCam>().Player.transform.position;
+                affectedPos.z = -10;
+                affectedRot = new Vector3();
+            }
+            if (Reset[0])
+                Translation = (affectedPos - go[0].transform.position) / 50F;
+            if (Reset[1])
+                Rotation = affectedRot - go[0].transform.rotation.eulerAngles;
+        }
+
+
+        int FPS = 0;
+        try { FPS = int.Parse(GameObject.Find("Base").transform.GetChild(1).GetComponent<Text>().text.Replace(" FPS", "")); } catch { }
         float Frame = Speed * FPS;
         if ((int)(Frame / 2) == Frame / 2 & Type == 1) Frame = Frame + 1; //Support Frame pair si le type est Fluid
         for (int i = 0; i < Frame; i++)
         {
-            GameObject[] go = null;
-            if (AffectationType == 0)
-            {
-                go = new GameObject[Blocks.Length];
-                for (int b = 0; b < Blocks.Length; b++)
-                    go[b] = GameObject.Find("Objet n° " + Blocks[b]);
-            }
-            else if (AffectationType == 1)
-            {
-                go = new GameObject[] { GameObject.Find("Main Camera") };
-                go[0].GetComponent<MainCam>().OnPlayer = false;
-            }
-
             for (int b = 0; b < go.Length; b++)
             {
-                if (Reset[0] & AffectationType == 0)
-                    try { Translation = GameObject.Find("Main Camera").GetComponent<LevelPlayer>().GetObjectPos(int.Parse(Blocks[b])) - go[b].transform.position; } catch { }
-                else if(Reset[0] & AffectationType == 1)
-                    Translation = GameObject.Find("Main Camera").GetComponent<MainCam>().Player.transform.position - go[b].transform.position;
-                if (Reset[1] & AffectationType == 0)
-                    try { Rotation = Editor_MoveTrigger.getVector3(GameObject.Find("Main Camera").GetComponent<LevelPlayer>().GetBlocStatus("Rotation", int.Parse(Blocks[b]))) - go[b].transform.rotation.eulerAngles; } catch { }
-                else if (Reset[0] & AffectationType == 1)
-                    Rotation = go[b].transform.rotation.eulerAngles - GameObject.Find("Main Camera").GetComponent<MainCam>().Player.transform.rotation.eulerAngles;
                 Vector2 moveVector = new Vector2();
                 if (Type == 0) moveVector = Translation / Frame;
                 else if (Type == 1)
@@ -77,14 +91,14 @@ public class MoveTrigger : MonoBehaviour
                 if (go != null & Frame >= 1)
                 {
                     Vector3 pos = go[b].transform.position;
-                    
-                        for (int m = 0; m < 2; m++)
-                        {
-                            if (TranslationFromPlayer[m])
-                                pos[m] = GameObject.Find("Main Camera").GetComponent<MainCam>().Player.transform.position[m] + (Translation[m] * 50);
-                            else pos[m] = pos[m] + moveVector[m] * 50;
-                        }
-                    
+
+                    for (int m = 0; m < 2; m++)
+                    {
+                        if (TranslationFromPlayer[m])
+                            pos[m] = GameObject.Find("Main Camera").GetComponent<MainCam>().Player.transform.position[m] + (Translation[m] * 50);
+                        else pos[m] = pos[m] + moveVector[m] * 50;
+                    }
+
                     go[b].transform.position = pos;
 
                     Quaternion quaternion = new Quaternion();
