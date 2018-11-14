818adb9b27ff7ef393daf4f955efcd200d568f0d
EvanG <evan_g@orange.fr>
2018-11-14 16:47:38 +0100

    Support presque complet des APNG
    Fonctionnalités manquantes :
    - Dispose à None
    - Blend à Over

--- Assets/Scripts/Images/Sprite_API.cs
+++ Assets/Scripts/Images/Sprite_API.cs
@@ -1,7 +1,7 @@
 ﻿using LibAPNG;
-using System.Collections;
-using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace Sprite_API
@@ -9,26 +9,43 @@ namespace Sprite_API
     public class Sprite_API_Data
     {
         public Sprite[] Frames;
-        public float FramePerSeconds;
+        public float[] Delay;
+        public uint Repeat;
     }
 
     public class Sprite_API : MonoBehaviour
-    {
-
-        static string spritesPath { get { return Application.persistentDataPath + "/Ressources/default/"; } }
+    {        
+        public static string spritesPath = Application.persistentDataPath + "/Ressources/default/textures/";
 
         public static Sprite_API_Data GetSprites(string id, bool isPath = false)
         {
-            string filePath = spritesPath + id + ".png";
+            string filePath = id;
+            if(!isPath) filePath = spritesPath + id + ".png";
+
             if (File.Exists(filePath))
             {
                 APNG apng = new APNG(filePath);
-                Sprite[] Frames;
-                float FramesPerSeconds = 0;
+                float[] Delay = new float[apng.Frames.Length];
+                Sprite[] Frames = new Sprite[apng.Frames.Length];
+
+                /*Bitmap[] frames = UpdateUI(apng);
+                for (int i = 0; i < frames.Length; i++)
+                {
+                    Texture2D tex = new Texture2D(1, 1);
+                    tex.LoadImage(ToByteArray(frames[i], System.Drawing.Imaging.ImageFormat.Png));
+                    tex.Apply();
+                    Frames[i] = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(.5f, .5f));
+
+                    if (apng.Frames[i].fcTLChunk.DelayNum == 0) Delay[i] = 10000;
+                    else if (apng.Frames[i].fcTLChunk.DelayDen == 0) Delay[i] = 100F / apng.Frames[i].fcTLChunk.DelayNum;
+                    else Delay[i] = (float)apng.Frames[i].fcTLChunk.DelayDen / apng.Frames[i].fcTLChunk.DelayNum;
+                    if (Delay[i] == 0) Delay[i] = 60;
+                }*/
 
                 if (apng.IsSimplePNG) //PNG
                 {
                     Frames = new Sprite[1];
+                    Delay = new float[1] { 0 };
                     Texture2D tex = new Texture2D(1, 1);
                     tex.LoadImage(File.ReadAllBytes(filePath));
                     Frames[0] = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(.5f, .5f));
@@ -36,53 +53,104 @@ namespace Sprite_API
                 else //APNG
                 {
                     Frames = new Sprite[apng.Frames.Length];
-                    if (apng.DefaultImage.fcTLChunk.DelayNum == 0) FramesPerSeconds = 10000;
-                    else if (apng.DefaultImage.fcTLChunk.DelayDen == 0) FramesPerSeconds = 100F / apng.DefaultImage.fcTLChunk.DelayNum;
-                    else FramesPerSeconds = (float)apng.DefaultImage.fcTLChunk.DelayDen / apng.DefaultImage.fcTLChunk.DelayNum;
-                    if (FramesPerSeconds == 0) FramesPerSeconds = 60;
+                    Delay = new float[apng.Frames.Length];
 
+                    APNGFrameInfo info = new APNGFrameInfo(id, apng);
                     for (int i = 0; i < apng.Frames.Length; i++)
-                        Frames[i] = GetSprite(apng, i);
+                    {
+                        if (apng.Frames[i].fcTLChunk.DelayNum == 0) Delay[i] = 10000;
+                        else if (apng.Frames[i].fcTLChunk.DelayDen == 0) Delay[i] = 100F / apng.Frames[i].fcTLChunk.DelayNum;
+                        else Delay[i] = (float)apng.Frames[i].fcTLChunk.DelayDen / apng.Frames[i].fcTLChunk.DelayNum;
+                        if (Delay[i] == 0) Delay[i] = 60;
+
+                        info.index = i;
+                        Frames[i] = GetSprite(info);
+                    }
+
+                    for (int i = 0; i < info.errors.Length; i++)
+                        BaseControl.LogNewMassage(info.errors[i]);
                 }
 
                 Sprite_API_Data SAD = new Sprite_API_Data();
                 SAD.Frames = Frames;
-                SAD.FramePerSeconds = FramesPerSeconds;
+                SAD.Delay = Delay;
+                SAD.Repeat = apng.acTLChunk.NumPlays;
                 return SAD;
             }
             else return null;
         }
-        public static Sprite GetSprite(APNG apng, int index)
+
+        public class APNGFrameInfo
         {
-            Frame frame = apng.Frames[index];
+            public string id { get; set; }
+            public APNG apng { get; set; }
+            public int index { get; set; }
+            public Texture2D buffer { get; set; }
+            public DisposeOps dispose { get; set; }
+            public string[] errors { get; set; }
+
+            public APNGFrameInfo(string identifier, APNG png, int i = 0)
+            {
+                id = identifier;
+                apng = png;
+                index = i;
+                buffer = CreateTransparent(png.IHDRChunk.Width, png.IHDRChunk.Height);
+                dispose = apng.DefaultImage.fcTLChunk.DisposeOp;
+                errors = new string[0];
+            }
+        }
+        
+        public static Sprite GetSprite(APNGFrameInfo info)
+        {
+            Frame frame = info.apng.Frames[info.index];
 
             Texture2D text = new Texture2D(1, 1);
             text.LoadImage(frame.GetStream().ToArray());
 
-            Texture2D tex = CreateTransparent((int)apng.DefaultImage.fcTLChunk.Width, (int)apng.DefaultImage.fcTLChunk.Height);
+            info.buffer.SetPixels((int)frame.fcTLChunk.XOffset, info.buffer.height - (int)frame.fcTLChunk.YOffset - text.height,
+                text.width, text.height, text.GetPixels(0, 0, text.width, text.height));
 
-
-            tex.SetPixels((int)frame.fcTLChunk.XOffset, tex.height - (int)frame.fcTLChunk.YOffset - text.height, text.width, text.height,
-                text.GetPixels(0, 0, text.width, text.height));
+            Texture2D tex = info.buffer;
             tex.Apply();
+
+            info.dispose = frame.fcTLChunk.DisposeOp;
+            TamponCleaner(info);
 
             return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(.5f, .5f));
         }
 
+        static void TamponCleaner(APNGFrameInfo info)
+        {
+            if (info.index == 0 & info.dispose == DisposeOps.APNGDisposeOpPrevious) info.dispose = DisposeOps.APNGDisposeOpBackground;
+
+            if (info.dispose == DisposeOps.APNGDisposeOpPrevious)
+            {
+                Texture2D text = new Texture2D(1, 1);
+                text.LoadImage(info.apng.Frames[info.index - 1].GetStream().ToArray());
+
+                info.buffer = CreateTransparent(info.apng.IHDRChunk.Width, info.apng.IHDRChunk.Height);
+                Frame frame = info.apng.Frames[info.index - 1];
+                info.buffer.SetPixels((int)frame.fcTLChunk.XOffset, info.buffer.height - (int)frame.fcTLChunk.YOffset - text.height, text.width, text.height,
+                    text.GetPixels(0, 0, text.width, text.height));
+            }
+            else if (info.dispose == DisposeOps.APNGDisposeOpBackground)
+                info.buffer = CreateTransparent(info.apng.IHDRChunk.Width, info.apng.IHDRChunk.Height);
+            else if (info.dispose == DisposeOps.APNGDisposeOpNone)
+                info.errors = info.errors.Union(new string[] { "The texture with id \"" + info.id + "\" contains frames with the dispose value set to None, the game does not support it yet !" }).ToArray();
+        }
 
         static Texture2D CreateTransparent(int width, int height)
         {
-            Texture2D tex = new Texture2D(width, height, TextureFormat.ARGB32, false);
-            UnityEngine.Color fillColor = UnityEngine.Color.clear;
-            UnityEngine.Color[] fillPixels = new UnityEngine.Color[tex.width * tex.height];
-            for (int v = 0; v < fillPixels.Length; v++)
-            {
-                fillPixels[v] = fillColor;
-            }
-            tex.SetPixels(fillPixels);
-            tex.Apply();
+            Texture2D texture_ = new Texture2D(width, height);
+            
+            Color32 resetColor = UnityEngine.Color.white;
+            Color32[] resetColorArray = texture_.GetPixels32();
+            for (int i = 0; i < resetColorArray.Length; i++)
+                resetColorArray[i] = resetColor;
 
-            return tex;
+            texture_.SetPixels32(resetColorArray);
+            texture_.Apply();
+            return texture_;
         }
     }
 }
---
+++ Assets/Scripts/Images/UImage_Reader.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImage_Reader : MonoBehaviour
+{
+    //Configuration
+    public string id = "";
+    public bool ASync = true;
+
+    //Parameters
+    Sprite[] Frames;
+    float[] Delay = new float[] { 60 };
+    uint MaxPlayed;
+
+    //Data
+    Image component;
+    uint Played = 0;
+    public int Frame;
+
+
+    void Start()
+    {
+        Sprite_API.Sprite_API_Data data = null;
+        /*if (ASync)
+        {
+            Task.Run(async () =>
+            {
+                Task<Sprite_API.Sprite_API_Data> ASyncMethode = StartASync();
+                await Task.WhenAny(ASyncMethode);
+                data = ASyncMethode.Result;
+            });
+        }
+        else */data = Sprite_API.Sprite_API.GetSprites(id);
+
+        component = GetComponent<Image>();
+
+        if (data == null)
+        {
+            Frames = new Sprite[] { component.sprite };
+            Delay = new float[] { 60F };
+            MaxPlayed = 0;
+        }
+        else
+        {
+            Frames = data.Frames;
+            Delay = data.Delay;
+            MaxPlayed = data.Repeat;
+        }
+
+        StartAnimating();
+
+    }
+
+    async Task<Sprite_API.Sprite_API_Data> StartASync()
+    {
+        string path = Sprite_API.Sprite_API.spritesPath + id + ".png";
+        return await Task.Run(() =>
+        {
+            return Sprite_API.Sprite_API.GetSprites(path, true);
+        });
+    }
+
+    public void StartAnimating()
+    {
+        if (Frames.Length > 1)
+        {
+            Frame = 0;
+            Played = 0;
+            StartCoroutine(APNG());
+        }
+        else if (Frames.Length == 1)
+            component.sprite = Frames[0];
+    }
+    public void StopAnimating()
+    {
+        if (Frames.Length > 1)
+        {
+            StopCoroutine(APNG());
+            Frame = -1;
+        }
+        else if (Frames.Length == 1)
+            component.sprite = Frames[0];
+    }
+
+    IEnumerator APNG()
+    {
+        if (Frame < Frames.Length - 1)
+            Frame = Frame + 1;
+        else if (Frame != -1)
+        {
+            Frame = 0;
+            Played++;
+        }
+
+        if (Played < MaxPlayed | MaxPlayed == 0)
+        {
+            float Speed = 1F / Delay[Frame];
+            yield return new WaitForSeconds(Speed);
+            component.sprite = Frames[Frame];
+            StartCoroutine(APNG());
+        }
+        else StopAnimating();
+    }
+}
