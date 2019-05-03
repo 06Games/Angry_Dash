88c98bc57a5dce74be416227e05d6ea5250b31d7
EvanG <evan_g@orange.fr>
2019-05-03 15:52:01 +0200

    Automatisation CMD

---
+++ scripts/Update RPs.cmd
@@ -0,0 +1,9 @@
+set Git=C:\Users\evan\Documents\GitHub\Angry-Dash
+set AD=%APPDATA%\..\LocalLow\06Games\Angry Dash\Ressources
+
+echo O | del "%AD%\default"
+xcopy "%Git%\default" "%AD%\default" /e /Q /Y /i
+echo O | del "%AD%\light"
+xcopy "%Git%\light" "%AD%\light" /e /Q /Y /i
+echo O | del "%AD%\oled"
+xcopy "%Git%\oled" "%AD%\oled" /e /Q /Y /i
