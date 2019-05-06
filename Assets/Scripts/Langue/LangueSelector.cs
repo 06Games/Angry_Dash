17fd881aad6134bfb6fd3780c2779426b1f28ef5
EvanG <evan_g@orange.fr>
2019-05-06 20:41:00 +0200

    Les drapeaux sont pleinement compatible RP + Nouveaux IDs dans le sous-menu Informations

--- Assets/Scripts/Langue/LangueSelector.cs
+++ Assets/Scripts/Langue/LangueSelector.cs
@@ -19,17 +19,14 @@ public class LangueSelector : MonoBehaviour
 
     void NewStart()
     {
-        if (string.IsNullOrEmpty(LangueAPI.selectedLanguage))
-            ReloadScene();
+        if (string.IsNullOrEmpty(LangueAPI.selectedLanguage)) ReloadScene();
 
         if (Langues != null)
-            if (Langues.childCount <= 1)
-                GetLangDispo(false);
+            if (Langues.childCount <= 1) GetLangDispo();
 
         for (int i = 0; i < LangueDispo.Length; i++)
         {
-            if (LangueAPI.selectedLanguage == LangueDispo[i])
-                actuel = i;
+            if (LangueAPI.selectedLanguage == LangueDispo[i]) actuel = i;
         }
     }
 
@@ -38,23 +35,18 @@ public class LangueSelector : MonoBehaviour
         if (Langues != null & LangFlag != null)
         {
             for (int i = 0; i < LangueDispo.Length & i < Langues.childCount - 1; i++)
-            {
-                if (i == actuel)
-                    Langues.GetChild(i + 1).GetChild(0).gameObject.SetActive(true);
-                else Langues.GetChild(i + 1).GetChild(0).gameObject.SetActive(false);
-            }
+                Langues.GetChild(i + 1).GetComponent<Button>().interactable = i != actuel;
         }
     }
 
     public void Chang(int i)
     {
-        if (i != actuel)
-            actuel = i;
+        if (i != actuel) actuel = i;
     }
     public void ReloadScene() { if (LangueAPI.selectedLanguage == LangueDispo[actuel]) return; Apply(); LS.LoadScreen(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, new string[] { "Settings", "Langues" }); }
     void Apply() { if (LangueDispo.Length > actuel) LangueAPI.selectedLanguage = LangueDispo[actuel]; else Debug.LogError((actuel + 1) + " is more than the number of language file : " + LangueDispo.Length); }
 
-    void GetLangDispo(bool forceUpdate = false)
+    void GetLangDispo()
     {
         string[] languages = Directory.GetFiles(Application.persistentDataPath + "/Ressources/default/languages/native/");
         LangFlag = new Sprite[languages.Length];
@@ -65,25 +57,15 @@ public class LangueSelector : MonoBehaviour
             int dispo = i;
             go.GetComponent<Button>().onClick.RemoveAllListeners();
             go.GetComponent<Button>().onClick.AddListener(() => Chang(dispo));
-
-            if (File.Exists(Sprite_API.Sprite_API.spritesPath("native/GUI/settingsMenu/languages/" + Path.GetFileNameWithoutExtension(languages[i]) + ".png")))
-            {
-                Texture2D tex = new Texture2D(1, 1);
-                tex.LoadImage(File.ReadAllBytes(Sprite_API.Sprite_API.spritesPath("native/GUI/settingsMenu/languages/" + Path.GetFileNameWithoutExtension(languages[i]) + ".png")));
-                LangFlag[i] = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(.5f, .5f));
-            }
-            else LangFlag[i] = Langues.GetChild(0).GetComponent<Image>().sprite;
-            go.GetComponent<Image>().sprite = LangFlag[i];
+            go.GetComponent<UImage_Reader>().SetID("native/GUI/settingsMenu/languages/" + Path.GetFileNameWithoutExtension(languages[i])).Load();
             go.gameObject.SetActive(true);
         }
 
-        if (LangueAPI.selectedLanguage == null)
-            Apply();
+        if (LangueAPI.selectedLanguage == null) Apply();
 
         for (int i = 0; i < LangueDispo.Length; i++)
         {
-            if (LangueAPI.selectedLanguage == LangueDispo[i])
-                actuel = i;
+            if (LangueAPI.selectedLanguage == LangueDispo[i]) actuel = i;
         }
     }
 }
