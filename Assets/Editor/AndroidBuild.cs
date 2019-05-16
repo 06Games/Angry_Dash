b7ac8a29c00fde6949dfe775dc278fd216fd4797
EvanG <evan_g@orange.fr>
2019-05-16 21:30:35 +0200

    Suppression auto des symbols

---
+++ Assets/Editor/AndroidBuild.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class AndroidBuild : MonoBehaviour
+{
+
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        if (buildTarget == BuildTarget.Android)
+        {
+            FileInfo build = new FileInfo(pathToBuiltProject);
+            string symbolsPath = build.DirectoryName + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(build.FullName) + "-" + Application.version + "-v" + PlayerSettings.Android.bundleVersionCode + ".symbols.zip";
+            File.Delete(symbolsPath);
+        }
+    }
+}
