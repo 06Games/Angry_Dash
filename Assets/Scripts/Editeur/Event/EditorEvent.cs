ff6e8ff8f9b4123110697189dbb10e544b57d503
EvanG <evan_g@orange.fr>
2019-07-02 20:20:49 +0200

    Déplacement des scripts gérant les events dans l'éditeur

---
+++ Assets/Scripts/Editeur/Event/EditorEvent.cs
@@ -0,0 +1,234 @@
+﻿using AngryDash.Image.Reader;
+using System.Linq;
+using Tools;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AngryDah.Editor.Event
+{
+    public class EditorEvent : MonoBehaviour
+    {
+        public Editeur editor;
+        public enum ProgType { visual, textual }
+        public ProgType type = ProgType.visual;
+
+        public void ChangeType(int argument) { ChangeType((ProgType)argument); }
+        public void ChangeType(ProgType newType, bool save = true)
+        {
+            if (save)
+            {
+                //Save
+                if (type == ProgType.visual)
+                {
+                    VisualSave();
+                    foreach (Transform child in transform.GetChild(1).GetChild(1)) Destroy(child.gameObject);
+                }
+                else if (type == ProgType.textual) TextualSave();
+            }
+
+            type = newType; //Set the new type
+
+            //Load
+            if (type == ProgType.visual) UnityThread.executeInUpdate(() => VisualInitialization());
+            else if (type == ProgType.textual) TextualInitialization();
+        }
+
+        void OnEnable() { editor.bloqueSelect = true; ChangeType(type, false); }
+        public void Exit()
+        {
+            if (type == ProgType.visual)
+            {
+                VisualSave();
+                foreach (Transform child in transform.GetChild(1).GetChild(1)) Destroy(child.gameObject);
+            }
+            else if (type == ProgType.textual) TextualSave();
+
+            foreach (int id in editor.SelectedBlock)
+            {
+                Transform obj = editor.transform.GetChild(1).Find("Objet n° " + id);
+                if (obj != null) obj.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            editor.SelectedBlock = new int[0];
+
+            editor.bloqueSelect = false;
+        }
+
+        #region Visual
+        System.Collections.Generic.Dictionary<string, EditorEventItem> visualPrefabs = new System.Collections.Generic.Dictionary<string, EditorEventItem>();
+
+        void VisualInitialization()
+        {
+            Transform visual = transform.GetChild(1);
+            Transform elements = visual.GetChild(0).GetComponent<ScrollRect>().content;
+
+            string[] ids = new string[] {
+                "collision", //trigger
+                "teleport", //action
+                "if" //condition
+            };
+            foreach (string id in ids)
+            {
+                GameObject config = Resources.Load<GameObject>($"Events/{id}");
+                if (config != null)
+                {
+                    if (!visualPrefabs.ContainsKey(id))
+                    {
+                        GameObject Slot = Instantiate(elements.GetChild(0).gameObject, elements);
+                        EditorEventItem Item = Instantiate(config, Slot.transform).GetComponent<EditorEventItem>();
+
+                        Slot.name = Item.id = id;
+                        Slot.GetComponent<GridLayoutGroup>().cellSize =
+                            Slot.GetComponent<RectTransform>().sizeDelta =
+                            Item.referenceSize =
+                            Item.GetComponent<RectTransform>().sizeDelta;
+                        Slot.GetComponent<UImage_Reader>().baseID = Item.GetComponent<UImage_Reader>().baseID;
+                        Slot.SetActive(true);
+
+                        visualPrefabs.Add(id, Item);
+                    }
+                }
+#if UNITY_EDITOR
+                else Debug.LogWarning($"<b>{id}</b> has no prefab");
+#endif
+            }
+
+            if(editor.SelectedBlock.Length == 1) VisualParse(editor.GetBlocStatus("Script", editor.SelectedBlock[0]));
+        }
+
+        public void VisualSave()
+        {
+            string script = $"// Auto-generated script from the visual programming panel\n\n{VisualToScript(transform.GetChild(1).GetChild(1))}";
+            editor.ChangBlocStatus("Script", script, editor.SelectedBlock);
+        }
+        private string VisualToScript(Transform field, string prefix = "")
+        {
+            StringBuilder script = new StringBuilder(prefix) { appendEmptyStrings = false };
+            foreach (Transform go in field)
+            {
+                if (script.Length > 0) script.AppendLine();
+
+                EditorEventItem item = go.GetComponent<EditorEventItem>();
+                if (item.type == EditorEventItem.Type.trigger)
+                {
+                    script.AppendLine($"void {item.id}()");
+                    script.AppendLine("{");
+                    foreach (EventField childField in item.fields)
+                    {
+                        if (childField.transform.childCount > 0) script.AppendLine(VisualToScript(childField.transform, "\t"));
+                    }
+                    script.AppendLine("}");
+                }
+                else if (item.type == EditorEventItem.Type.action) script.AppendLine($"{item.id}()");
+                else if (item.type == EditorEventItem.Type.conditional)
+                {
+                    string condition = "";
+                    StringBuilder actions = new StringBuilder() { appendEmptyStrings = false };
+                    foreach (EventField childField in item.fields)
+                    {
+                        if (childField.accepted == EditorEventItem.Type.logicalOperator)
+                        {
+                            if (!string.IsNullOrEmpty(condition)) condition += ", ";
+                            condition += VisualToScript(childField.transform);
+                        }
+                        else
+                        {
+                            if (childField.id != "then") actions.AppendLine(childField.id);
+                            actions.AppendLine("{");
+                            if (childField.transform.childCount > 0) actions.AppendLine(VisualToScript(childField.transform, "\t"));
+                            actions.AppendLine("}");
+                        }
+                    }
+                    script.AppendLine($"if ({condition})");
+                    script.Merge(actions);
+                }
+                else if (item.type == EditorEventItem.Type.logicalOperator)
+                    throw new System.NotImplementedException("Logical operators are not supported for the moment");
+            }
+            return script.ToString();
+        }
+
+        void VisualParse(string script)
+        {
+            string[] lines = script.Split("\n");
+            Transform topParent = transform.GetChild(1).GetChild(1);
+            Transform parent = topParent;
+
+            Transform lastParent = null;
+            Transform lastObj = null;
+            string fieldID = null;
+            foreach (string line in lines)
+            {
+                if (line.Contains("void ")) //Trigger
+                    SpawnObj(line.Remove(line.LastIndexOf("(")).Remove(0, "void ".Length));
+                else if (line.Contains("{")) parent = lastObj;
+                else if (line.Contains("}"))
+                {
+                    EditorEventItem parentItem = parent.GetComponent<EditorEventItem>();
+                    UnityThread.executeInUpdate(() => parentItem.UpdateSize());
+                    lastParent = parent;
+                    parent = parent.parent != topParent ? parent.parent.parent.parent : parent.parent;
+                    fieldID = null;
+                }
+                else if (line.Contains("if (")) //Condition
+                {
+                    SpawnObj("if");
+                    fieldID = "then";
+                }
+                else if (line.Contains("else"))
+                { 
+                    parent = lastParent; //Restore the IF
+                    fieldID = "else";
+                }
+                else if (line.Contains("()")) //Action
+                    SpawnObj(line.Remove(line.LastIndexOf("(")));
+            }
+
+            GameObject SpawnObj(string id)
+            {
+                if (visualPrefabs.ContainsKey(id))
+                {
+                    EditorEventItem prefab = visualPrefabs[id];
+                    Transform objParent = parent;
+
+                    if (parent != topParent)
+                    {
+                        EditorEventItem parentItem = parent.GetComponent<EditorEventItem>();
+                        EventField field = parentItem.fields.Where(f => f.CanDrop(prefab.type) & (f.id == fieldID | fieldID == null)).FirstOrDefault();
+                        if (field != null) objParent = field.transform;
+                    }
+                    GameObject go = Instantiate(prefab.gameObject, objParent);
+                    go.SetActive(true);
+                    lastObj = go.transform;
+                    return go;
+                }
+                else return null;
+            }
+
+            UnityThread.executeInUpdate(() =>
+            {
+                Vector2 lastPos = new Vector2(25, -25);
+                foreach (RectTransform transform in topParent)
+                {
+                    transform.anchoredPosition = transform.rect.size * new Vector2(0.5F, -0.5F) + lastPos;
+                    lastPos.x += transform.rect.size.x + 25;
+                }
+            });
+        }
+        #endregion
+
+        #region Textual
+        public void TextualSave()
+        {
+            string script = transform.GetChild(2).GetChild(1).GetComponent<InputField>().text;
+            editor.ChangBlocStatus("Script", script, editor.SelectedBlock); 
+        }
+
+        void TextualInitialization()
+        {
+            string script = "";
+            if (editor.SelectedBlock.Length == 1) script = editor.GetBlocStatus("Script", editor.SelectedBlock[0]);
+            transform.GetChild(2).GetChild(1).GetComponent<InputField>().text = script;
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Editeur/Event/EditorEventDragHandler.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Tools;
+
+namespace AngryDah.Editor.Event
+{
+    public class EditorEventDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public static EditorEventItem itemBeingDragged;
+        [HideInInspector] public Transform startParent;
+        public Transform visualPanel;
+
+        void Start()
+        {
+            if (startParent == null) startParent = transform.parent;
+            if (visualPanel == null) visualPanel = transform.FindParent("Visual");
+
+            if (startParent.childCount == 1 & transform.IsChildOf(visualPanel.GetChild(0)))
+            {
+                gameObject.SetActive(false);
+                Instantiate(startParent.GetChild(0).gameObject, startParent).SetActive(true);
+            }
+        }
+
+        //When the user grab the item
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            startParent = transform.parent;
+            itemBeingDragged = GetComponent<EditorEventItem>();
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+
+        //During the traget
+        public void OnDrag(PointerEventData eventData) { transform.position = eventData.position; /*Follow the mouse*/ }
+
+        //When the user release the item
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            itemBeingDragged = null;
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+            if (startParent.IsChildOf(visualPanel.GetChild(0).GetComponent<UnityEngine.UI.ScrollRect>().content)) //The item has just been spawned
+            {
+                if (visualPanel.GetChild(1).GetComponent<RectTransform>().IsOver((RectTransform)transform)) //The item is over the code panel
+                {
+                    if (!transform.parent.GetComponent<RectTransform>().IsOver(transform.position)) transform.SetParent(visualPanel.GetChild(1));
+                    Instantiate(startParent.GetChild(0).gameObject, startParent).SetActive(true);
+                }
+                else
+                {
+                    Instantiate(startParent.GetChild(0).gameObject, startParent).SetActive(true);
+                    if (startParent.childCount > 2 & transform.parent != startParent) Destroy(gameObject);
+                }
+
+                if (startParent.childCount > 2)
+                {
+                    for (int i = 2; i < startParent.childCount; i++)
+                        Destroy(startParent.GetChild(2).gameObject);
+                }
+            }
+            else if (!visualPanel.GetChild(1).GetComponent<RectTransform>().IsOver(transform.position)) //The item isn't hover the code panel
+            {
+                if (transform.parent.IsChildOf(visualPanel.GetChild(1)) & visualPanel.GetChild(1) != transform.parent) //The item was in the code
+                {
+                    transform.SetParent(visualPanel.GetChild(1)); //Remove the item from its parent
+                    startParent.GetComponentInParent<EditorEventItem>().UpdateSize(); //Refresh the parent
+                }
+                Destroy(gameObject); //Destroy the item
+            }
+            else if (startParent == transform.parent & !RectTransformExtensions.IsOver((RectTransform)startParent, transform.position)) //The item has exit its parent
+            {
+                transform.SetParent(visualPanel.GetChild(1));
+                startParent.GetComponentInParent<EditorEventItem>().UpdateSize();
+            }
+        }
+    }
+}
