3ca378d33fbc7ba6cb9269d78fd0ff064a0f5ee3
EvanG <evan_g@orange.fr>
2019-07-04 10:19:28 +0200

    Fix: Les coordonnées de destination sont correctement calculées

--- Assets/Scripts/Components/Event/Actions/PlayerUtilities.cs
+++ Assets/Scripts/Components/Event/Actions/PlayerUtilities.cs
@@ -4,6 +4,6 @@ namespace AngryDash.Game.Event.Action
 {
     public class PlayerUtilities
     {
-        public static void Teleport(double x, double y) { Player.userPlayer.transform.position = new Vector2((float)x * 50F, (float)y * 50F); }
+        public static void Teleport(double x, double y) { Player.userPlayer.transform.position = new Vector2((float)x * 50F + 25, (float)y * 50F + 25); }
     }
 }
