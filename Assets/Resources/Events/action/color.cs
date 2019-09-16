2876273a54fd64534f42d086740bbb64d4d4762f
EvanG <evan_g@orange.fr>
2019-09-16 19:31:24 +0200

    ColorPicker sur le bloc de code couleur

---
+++ Assets/Resources/Events/action/color.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AngryDash.Editor.Event.Components
+{
+    public class color : MonoBehaviour
+    {
+        public InputField R;
+        public InputField G;
+        public InputField B;
+
+        public ColorPicker picker;
+        private GameObject pickedDisablePanel;
+
+        void Start() { pickedDisablePanel = picker.transform.Find("Disabled").gameObject; }
+        void Update() { pickedDisablePanel.SetActive(!R.interactable); }
+
+        public void rgbChanged()
+        {
+            Color32 color = new Color32();
+            byte.TryParse(R.text, out color.r);
+            byte.TryParse(G.text, out color.g);
+            byte.TryParse(B.text, out color.b);
+            picker.CurrentColor = color;
+        }
+        public void pickerChanged()
+        {
+            Color32 color = picker.CurrentColor;
+            R.text = color.r.ToString();
+            G.text = color.g.ToString();
+            B.text = color.b.ToString();
+        }
+    }
+}
