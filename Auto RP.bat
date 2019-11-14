2e9d9d12b0fd536eac9f93df15ce9340b35e7be5
EvanG <evan_g@orange.fr>
2019-11-14 11:33:14 +0100

    L'application automatique des RPs fonctionne correctement

--- Auto RP.bat
+++ Auto RP.bat
@@ -1,9 +1,9 @@
 set Git=%userprofile%\Documents\GitHub\Angry-Dash
 set AD=%APPDATA%\..\LocalLow\06Games\Angry Dash\Ressources
 
-echo O | del "%AD%\default"
+rmdir /Q /S "%AD%\default"
 xcopy "%Git%\default" "%AD%\default" /e /Q /Y /i
-echo O | del "%AD%\light"
+rmdir /Q /S "%AD%\light"
 xcopy "%Git%\light" "%AD%\light" /e /Q /Y /i
-echo O | del "%AD%\oled"
+rmdir /Q /S "%AD%\oled"
 xcopy "%Git%\oled" "%AD%\oled" /e /Q /Y /i
