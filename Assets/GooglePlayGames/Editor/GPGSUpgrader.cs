06f9558bad25fc2075535ead2ba1d48b4cf9da5b
EvanG <evan_g@orange.fr>
2019-11-14 11:34:06 +0100

    Le GPGSUpgrader ne log plus

--- Assets/GooglePlayGames/Editor/GPGSUpgrader.cs
+++ Assets/GooglePlayGames/Editor/GPGSUpgrader.cs
@@ -33,7 +33,7 @@ namespace GooglePlayGames.Editor
         /// </summary>
         static GPGSUpgrader()
         {
-            Debug.Log("GPGSUpgrader start");
+            //Debug.Log("GPGSUpgrader start");
             string initialVer = GPGSProjectSettings.Instance.Get(GPGSUtil.LASTUPGRADEKEY, "00000");
             if (!initialVer.Equals(PluginVersion.VersionKey))
             {
@@ -100,7 +100,7 @@ namespace GooglePlayGames.Editor
             }
 
             AssetDatabase.Refresh();
-            Debug.Log("GPGSUpgrader done");
+            //Debug.Log("GPGSUpgrader done");
         }
 
         /// <summary>
